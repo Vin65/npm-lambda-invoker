c18d4919d2fa1a49a64e971147f3fc4600872e44
vaberay <10362370+vaberay@users.noreply.github.com>
2018-02-19 08:22:23 -0800

    Add main class [EP-1901]

---
+++ src/objects/LambdaInvoker.js
@@ -0,0 +1,41 @@
+'use strict';
+
+import 'babel-polyfill';
+
+const AWS = require('aws-sdk');
+const offlineEnvironments = ['test', 'development'];
+
+const loggingCallback = function(err, data) {
+  console.log(err);
+  console.log(data);
+};
+
+class LambdaInvoker {
+  constructor(environment, region, handler, apiVersion) {
+    this.environment = environment;
+    this.region = region;
+    this.apiVersion = apiVersion || '2015-03-31';
+    this.handler = handler;
+    this.Lambda = new AWS.Lambda({ apiVersion: this.apiVersion, region: this.region });
+  }
+
+  invoke(params, callback, offlineAttributes) {
+    let offline = offlineEnvironments.includes(this.environment);
+    if (offline) { return this.invokeOffline(params, callback, offlineAttributes); }
+
+    return this.Lambda.invoke(params, loggingCallback);
+  }
+
+  invokeOffline(params, callback, offlineAttributes) {
+    let functionName = this.parseFunctionName(params.FunctionName);
+    let handlerFunction = this.handler[functionName];
+    handlerFunction(offlineAttributes.event, offlineAttributes.context, callback);
+  };
+
+  parseFunctionName(functionName) {
+    let nameArray = functionName.split('-');
+    return nameArray[nameArray.length - 1];
+  };
+};
+
+module.exports = LambdaInvoker;
